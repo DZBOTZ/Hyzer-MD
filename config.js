b3b10fc3e1810cac6c2bc120dbf304bcb5db8875
DZ EVERYDAY <98388114+DZBOTZ@users.noreply.github.com>
2022-07-31 10:52:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ global.web = 'https://saweria.com/anakkeren' //ubah jadi website lu, bisa link i
 global.price1 = '1 bulan = 10.000\n2 Minggu 7.000\n1 Minggu 5.000'
 
 //=======『 Tampilan Dan Lainnya 』============//
-global.fotonya1 = 'https://telegra.ph/file/e4a2f4339da8a32ad20a1.jpg' //ganti jadi foto bot mu
+global.fotonya1 = 'https://telegra.ph/file/7691d838083c5dc5f18e6.jpg' //ganti jadi foto bot mu
 global.fotonya2 = 'https://telegra.ph/file/8c471d45b6cedad43cf84.jpg' //ini juga ganti 
 global.lolkey = 'Papah-Chan' //biar mudah ngegantinya semisal apikeynya expired:v
 global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
